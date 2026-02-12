b35a053682e8e04e1696301ca0676b8c10ca8a9a
qvvonk <qvvonk@gmail.com>
2026-02-12 17:01:31 +0300

    small changes

--- tozip.sh
+++ tozip.sh
@@ -4,5 +4,5 @@ mkdir -p exec_plugin
 cp readme.md manifest.json -t exec_plugin/
 cp -r src exec_plugin/
 find exec_plugin -name "*__pycache__*" -exec rm -rf {} +
-zip -r chat_sync exec_plugin
+zip -r exec_plugin exec_plugin
 rm -rf exec_plugin
